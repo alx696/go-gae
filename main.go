695851d9c5c445dbc6446570ad716f9ce99f11b9
李小明 <alx696@gmail.com>
2019-11-02 23:12:44 +0800

    调整模板文件路径.

--- main.go
+++ main.go
@@ -6,21 +6,13 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"path/filepath"
 )
 
 var ip string
 
 func main() {
-	//获取程序目录
-	appDir, e := filepath.Abs(filepath.Dir(os.Args[0]))
-	if e != nil {
-		log.Fatal(e)
-	}
-	log.Println("程序目录:", appDir)
-
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		t, e := template.ParseFiles(fmt.Sprint(appDir, "/template/index.html"))
+		t, e := template.ParseFiles("./template/index.html")
 		if e != nil {
 			writer.WriteHeader(http.StatusInternalServerError)
 			_, _ = writer.Write([]byte(e.Error()))
