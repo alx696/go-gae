b9910bb32774e5576abcb0f356a02e078bbf032a
李小明 <alx696@gmail.com>
2019-11-02 22:54:22 +0800

    首页改用模板

--- main.go
+++ main.go
@@ -2,16 +2,32 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 )
 
 var ip string
 
 func main() {
+	//获取程序目录
+	appDir, e := filepath.Abs(filepath.Dir(os.Args[0]))
+	if e != nil {
+		log.Fatal(e)
+	}
+	log.Println("程序目录:", appDir)
+
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		_, _ = writer.Write([]byte(fmt.Sprintf("<h1 style=\"text-align: center;\">%s</h1>", ip)))
+		t, e := template.ParseFiles(fmt.Sprint(appDir, "/template/index.html"))
+		if e != nil {
+			writer.WriteHeader(http.StatusInternalServerError)
+			_, _ = writer.Write([]byte(e.Error()))
+			return
+		}
+
+		_ = t.Execute(writer, ip)
 	})
 
 	http.HandleFunc("/ip", func(writer http.ResponseWriter, request *http.Request) {
